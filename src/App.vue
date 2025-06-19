2ec0f95eb830d4de4d472da5860417c2342c4460
gimjin <kimseongrim@gmail.com>
2025-06-19 10:55:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
 import { ref } from 'vue'
 import AppHeader from '@/views/app-header/AppHeader.vue'
 
-const keepAliveInclude = ref<string[]>(['AppHome'])
-// const keepAliveExclude = ref<string[]>([])
+const keepAliveExclude = ref<string[]>([])
 </script>
 
 <template>
   <AppHeader />
   <RouterView v-slot="{ Component }">
     <template v-if="Component">
-      <KeepAlive :include="keepAliveInclude">
+      <KeepAlive :exclude="keepAliveExclude">
         <component :is="Component" />
       </KeepAlive>
     </template>
